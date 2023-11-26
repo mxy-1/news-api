5613dd78e14acd6205a02f4023e1181445660620
May Hoang <mayhoang@MBP-de-May.cust.communityfibre.co.uk>
2023-11-26 21:14:45 +0000

    remove comments

--- app.js
+++ app.js
@@ -23,22 +23,3 @@ app.use(psqlError)
 app.use(serverError)
 
 module.exports = app
-
-// be available on /api/topics.
-// add new topic.
-// Request body accepts:
-// an object in the form:
-// {
-//   "slug": "topic name here",
-//   "description": "description here"
-// } ✅
-// Responds with:
-// 201 - a topic object containing the newly added topic.  ✅
-// 400 bad reuqest when missing properties { ...} ✅
-// 400 bad request when properties not valid asd:"desc" ✅
-// 400 bad reqest when values not valid ✅
-// 400 bad requets - extra properties ✅
-
-// Consider what errors could occur with this endpoint, and make sure to test for them.
-
-// Remember to add a description of this endpoint to your /api endpoint. ✅
