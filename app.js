b82988bb41521b022c871ac72b584df3229c4a97
May Hoang <mayhoang@MBP-de-May.cust.communityfibre.co.uk>
2023-12-11 09:53:37 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const apiRouter = require("./routes/api-router")
 const express = require("express")
 
 const app = express()
+const cors = require('cors');
+
+app.use(cors())
 app.use(express.json())
 
 app.use("/api", apiRouter)
